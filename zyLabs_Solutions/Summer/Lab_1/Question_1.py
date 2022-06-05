41e9607debcf27037ce231f8ca817792b500d8fc
Dayu Wang <dwang@stchas.edu>
2022-06-04 21:52:23 -0500

    Lab 1 - Question 2

---
+++ zyLabs_Solutions/Summer/Lab_1/Question_1.py
@@ -0,0 +1,6 @@
+# Created by Dayu Wang (dwang@stchas.edu) on 2022-06-04
+
+# Last updated by Dayu Wang (dwang@stchas.edu) on 2022-06-04
+
+
+print("Epstein's lawyers \"not satisfied\" with autopsy conclusions.", end='')
