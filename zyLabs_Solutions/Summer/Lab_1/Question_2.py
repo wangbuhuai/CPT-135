1f0581ae7fc8af14fbc5fcf68b55f531e942c721
Dayu Wang <dwang@stchas.edu>
2022-06-05 07:45:14 -0500

    f

---
+++ zyLabs_Solutions/Summer/Lab_1/Question_2.py
@@ -0,0 +1,10 @@
+# Created by Dayu Wang (dwang@stchas.edu) on 2022-06-04
+
+# Last updated by Dayu Wang (dwang@stchas.edu) on 2022-06-04
+
+
+length = int(input("Please enter the length of the cuboid: "))
+width = int(input("Please enter the width of the cuboid: "))
+height = int(input("Please enter the height of the cuboid: "))
+volume = length * width * height
+print("Volume:", volume)
